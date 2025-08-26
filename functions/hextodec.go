4a39b75e30e4f248f1596450c2008c03253676ba
ahalmerbati <aaaalmerbati@gmail.com>
2025-08-27 02:09:37 +0300

    .

--- functions/hextodec.go
+++ functions/hextodec.go
@@ -1,13 +1,26 @@
-package functions  
+package functions
+
+import "strings"
 
 func HexToDec(content string) string {
+	contentCheck := strings.Contains(content, "(hex)")
+
+	if !(contentCheck) {
+		return content
+	}
+	
 	arr := []rune(content)
 
-	for i:= 0; i < len(arr); i++ {
-		if(arr[i] == '3') {
-			break
+	if contentCheck {
+		for i := 0; i < len(arr); i++ {
+			if string(arr[i:i+5]) == "(hex)" {
+				// for j:= 0; j < i; j++ {
+
+				// }
+			}
 		}
 	}
-
 	return string(arr)
 }
+	
+
