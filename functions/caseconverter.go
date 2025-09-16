b98ae965745513e67807078dcfcd108d262602f5
ahalmerbati <aaaalmerbati@gmail.com>
2025-09-16 15:32:46 +0300

    .

--- functions/caseconverter.go
+++ functions/caseconverter.go
@@ -11,25 +11,5 @@ func CaseConverter(content string) string {
 		return content
 	}
 
-	caseCmd, err := regexp.Compile(`((\b[a-zA-Z0-9']+\b(\s*['.,!?:;]\s*)){1,})\s*\((up|low|cap)\s*,?\s*(\d+)?\)`)
-	if err != nil {
-		fmt.Println("Error: Could not compile the regular expression.")
-		return content
-	}
-
-	var result strings.Builder
-	index := 0
-
-	found := caseCmd.FindAllStringSubmatchIndex(content, -1)
 	
-
-	if len(found) == 0 {
-		return content
-	}
-
-	for _, match := range found {
-		
-	}
-
-
 }
