1ca746d10ac717442d8604f3f6aa369a7984613d
ahalmerbati <aaaalmerbati@gmail.com>
2025-09-17 00:27:33 +0300

    .

--- functions/caseconverter.go
+++ functions/caseconverter.go
@@ -24,23 +24,38 @@ func CaseConverter(content string) string {
 	}
 
 	matches := cmdExpression.FindAllStringSubmatchIndex(content, -1)
-	wordsToApply := wordExpression.FindAllString(content, -1)
+	if len(matches) == 0 {
+		fmt.Println("Error: No valid commands found in the content.")
+		return content
+	}
 
 	var result strings.Builder
 	index := 0
 
 	for _, match := range matches {
+		wordsToApply := wordExpression.FindAllString(content[:match[0]], -1)
+
 		command := content[match[2]:match[3]]
 
 		if match[4] == -1 {
 			words := content[index:match[2]]
+
 			wordsToApply = append(wordsToApply, words)
+
 			lastWord := wordsToApply[len(wordsToApply)-1]
-			modifiedContent := utils.ApplySingleCase(lastWord, command)
-			result.WriteString(content[index:])
+
+			var modifiedWords []string
+
+			modifiedWord := utils.ApplySingleCase(lastWord, command)
+
+			modifiedWords = append(modifiedWords, modifiedWord)
+
+			modifiedContent := utils.ReplaceLastWords(content[:match[0]], modifiedWords, 1)
+
 			result.WriteString(modifiedContent)
+
 			index = match[1]
-		}
+		} 
 	}
 	result.WriteString(content[index:])
 	return result.String()
